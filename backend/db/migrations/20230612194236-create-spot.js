588f5f096b3067b50784434fadddf9e78cbbd0de
Will Campbell <wpcampbell1@gmail.com>
2023-06-12 18:45:13 -0400

    added schema option for production env

--- backend/db/migrations/20230612194236-create-spot.js
+++ backend/db/migrations/20230612194236-create-spot.js
@@ -2,6 +2,9 @@
 /** @type {import('sequelize-cli').Migration} */
 
 let options = {};
+if (process.env.NODE_ENV === 'production') {
+    options.schema = process.env.SCHEMA
+}
 module.exports = {
     async up(queryInterface, Sequelize) {
         await queryInterface.createTable('Spots', {
--- backend/db/migrations/20230612221151-create-booking.js
+++ backend/db/migrations/20230612221151-create-booking.js
@@ -1,6 +1,9 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
 let options = {};
+if (process.env.NODE_ENV === 'production') {
+    options.schema = process.env.SCHEMA
+}
 module.exports = {
     async up(queryInterface, Sequelize) {
         await queryInterface.createTable('Bookings', {
