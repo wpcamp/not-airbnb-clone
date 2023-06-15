14392eaaed6b47c907d030ce48fb244cdc6c39fe
Will Campbell <wpcampbell1@gmail.com>
2023-06-15 19:36:31 -0400

    define schema

--- backend/db/seeders/20230613010953-demo-spotimg.js
+++ backend/db/seeders/20230613010953-demo-spotimg.js
@@ -2,6 +2,9 @@
 
 /** @type {import('sequelize-cli').Migration} */
 let options = {};
+if (process.env.NODE_ENV === 'production') {
+    options.schema = process.env.SCHEMA; // define your schema in options object
+}
 module.exports = {
     up: async(queryInterface, Sequelize) => {
         options.tableName = 'SpotImages'
