cf3be1b60f478f468203e31159eecadb19948386
Will Campbell <wpcampbell1@gmail.com>
2023-06-30 16:17:06 -0400

    ProfileButton and Navigation update

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,32 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/session';
 
 function ProfileButton({ user }) {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
+    const [showMenu, setShowMenu] = useState(false);
+    const ulRef = useRef();
+    console.log(showMenu);
+    const openMenu = () => {
+        if (showMenu) return;
+        setShowMenu(true);
+    };
 
-  const logout = (e) => {
-    e.preventDefault();
-    dispatch(sessionActions.logout());
-  };
+    useEffect(() => {
+        if (!showMenu) return;
 
-  const ulClassName = "profile-dropdown";
+        const closeMenu = (e) => {
+            if (!ulRef.current.contains(e.target)) {
+                setShowMenu(false);
+            }
+        };
 
-  return (
-    <>
-      <button>
-        <i className="fas fa-user-circle" />
-      </button>
-      <ul className="profile-dropdown">
-        <li>{user.username}</li>
-        <li>{user.firstName} {user.lastName}</li>
-        <li>{user.email}</li>
-        <li>
-          <button onClick={logout}>Log Out</button>
-        </li>
-      </ul>
-    </>
-  );
+        document.addEventListener('click', closeMenu);
+
+        return () => document.removeEventListener("click", closeMenu);
+    }, [showMenu]);
+
+    const logout = (e) => {
+        e.preventDefault();
+        dispatch(sessionActions.logout());
+    };
+
+    const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+
+    return (
+        <>
+            <button onClick={openMenu}>
+                <i className="fas fa-user-circle" />
+            </button>
+            {showMenu && (<ul className={ulClassName} ref={ulRef}>
+                <li>{user.username}</li>
+                <li>{user.firstName} {user.lastName}</li>
+                <li>{user.email}</li>
+                <li>
+                    <button onClick={logout}>Log Out</button>
+                </li>
+            </ul>)}
+        </>
+    );
 }
 
 export default ProfileButton;
