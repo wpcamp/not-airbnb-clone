e096ee4ae0710618c0561fa168a9b2ee082bb0e1
Will Campbell <wpcampbell1@gmail.com>
2023-06-30 14:45:46 -0400

    Created LoginFormPage, finished csrf verification

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,6 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { restoreCSRF, csrfFetch } from './store/csrf';
 import configureStore from './store';
+import * as sessionActions from "./store/session";
 
 const store = configureStore();
 
@@ -15,6 +16,7 @@ if (process.env.NODE_ENV !== 'production') {
   restoreCSRF();
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 
