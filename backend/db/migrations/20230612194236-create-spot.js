a084dcba964748fa51b15d2ea1532254118a0903
Will Campbell <wpcampbell1@gmail.com>
2023-08-02 12:34:51 -0500

    updated spot description from string to text type

--- backend/db/migrations/20230612194236-create-spot.js
+++ backend/db/migrations/20230612194236-create-spot.js
@@ -55,7 +55,7 @@ module.exports = {
                 allowNull: false
             },
             description: {
-                type: Sequelize.STRING,
+                type: Sequelize.TEXT,
                 allowNull: false
             },
             price: {
--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -66,7 +66,7 @@ module.exports = (sequelize, DataTypes) => {
             allowNull: false
         },
         description: {
-            type: DataTypes.STRING,
+            type: DataTypes.TEXT,
             allowNull: false
         },
         price: {
