5705daffe28696bb8e7297a901c251b64893011c
Will Campbell <wpcampbell1@gmail.com>
2023-08-03 17:58:50 -0500

    updated seed data for spot img

--- backend/db/seeders/20230613010953-demo-spotimg.js
+++ backend/db/seeders/20230613010953-demo-spotimg.js
@@ -6,39 +6,131 @@ if (process.env.NODE_ENV === 'production') {
     options.schema = process.env.SCHEMA; // define your schema in options object
 }
 module.exports = {
-    up: async(queryInterface, Sequelize) => {
+    up: async (queryInterface, Sequelize) => {
         options.tableName = 'SpotImages'
         return queryInterface.bulkInsert(options, [{
-                spotId: 1,
-                url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Dartmouth_College_campus_2007-06-23_Alumni_Gymnasium_01.JPG/270px-Dartmouth_College_campus_2007-06-23_Alumni_Gymnasium_01.JPG',
-                preview: true
-            },
-            {
-                spotId: 2,
-                url: 'https://images.otstatic.com/prod1/29145482/2/large.jpg',
-                preview: true
-            },
-            {
-                spotId: 3,
-                url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Buckingham_Palace_aerial_view_2016_%28cropped%29.jpg/1200px-Buckingham_Palace_aerial_view_2016_%28cropped%29.jpg',
-                preview: true
-            },
-            {
-                spotId: 4,
-                url: 'https://images1.apartments.com/i2/P-ojx1Xv2unTJbGZnQSEDlqja9isZx6jSN0xdwzsq0M/111/99-federal-st-unit-505-portland-me-building-photo.jpg',
-                preview: true
-            },{
-                spotId: 5,
-                url: 'https://ssl.cdn-redfin.com/photo/180/bigphoto/899/1524899_0.jpg',
-                preview: true
-            }, {
-                spotId: 6,
-                url: 'https://a0.muscache.com/im/pictures/miso/Hosting-21409981/original/a8fa243d-dac8-4238-93e5-f7aa33072ff8.jpeg?im_w=1200',
-                preview: true
-            }
-        ], {})
+            spotId: 1,
+            url: 'https://i.imgur.com/kAwNyxF.png',
+            preview: true
+        }, {
+            spotId: 1,
+            url: 'https://i.imgur.com/MzRolQx.png',
+            preview: false
+        }, {
+            spotId: 1,
+            url: 'https://i.imgur.com/L559q3x.png',
+            preview: false
+        }, {
+            spotId: 1,
+            url: 'https://i.imgur.com/LaDuiJS.png',
+            preview: false
+        }, {
+            spotId: 1,
+            url: 'https://i.imgur.com/J9yA7M7.png',
+            preview: false
+        },{
+            spotId: 2,
+            url: 'https://i.imgur.com/2a0OkQm.png',
+            preview: true
+        }, {
+            spotId: 2,
+            url: 'https://i.imgur.com/EUFZT8B.png',
+            preview: false
+        }, {
+            spotId: 2,
+            url: 'https://i.imgur.com/vPceRA9.png',
+            preview: false
+        }, {
+            spotId: 2,
+            url: 'https://i.imgur.com/WnnubMj.png',
+            preview: false
+        }, {
+            spotId: 2,
+            url: 'https://i.imgur.com/JLg24Iy.png',
+            preview: false
+        },{
+            spotId: 3,
+            url: 'https://i.imgur.com/QQkXgWS.jpg',
+            preview: true
+        }, {
+            spotId: 3,
+            url: 'https://i.imgur.com/5Xx5YbG.png',
+            preview: false
+        }, {
+            spotId: 3,
+            url: 'https://i.imgur.com/5vT6EJ7.png',
+            preview: false
+        }, {
+            spotId: 3,
+            url: 'https://i.imgur.com/rkCMzbU.png',
+            preview: false
+        }, {
+            spotId: 3,
+            url: 'https://i.imgur.com/ATrr12P.jpg',
+            preview: false
+        }, {
+            spotId: 4,
+            url: 'https://i.imgur.com/pz4WoNI.png',
+            preview: true
+        }, {
+            spotId: 4,
+            url: 'https://i.imgur.com/G3uJWBT.png',
+            preview: false
+        }, {
+            spotId: 4,
+            url: 'https://i.imgur.com/8SYlSsn.png',
+            preview: false
+        }, {
+            spotId: 4,
+            url: 'https://i.imgur.com/EzMbs9L.png',
+            preview: false
+        }, {
+            spotId: 4,
+            url: 'https://i.imgur.com/FXnVqbV.png',
+            preview: false
+        }, {
+            spotId: 5,
+            url: 'https://i.imgur.com/Zln3Ub0.png',
+            preview: true
+        }, {
+            spotId: 5,
+            url: 'https://i.imgur.com/5Py7mk8.png',
+            preview: false
+        }, {
+            spotId: 5,
+            url: 'https://i.imgur.com/1gMcipP.png',
+            preview: false
+        }, {
+            spotId: 5,
+            url: 'https://i.imgur.com/Ie0LN5l.png',
+            preview: false
+        }, {
+            spotId: 5,
+            url: 'https://i.imgur.com/5ewApDT.png',
+            preview: false
+        }, {
+            spotId: 6,
+            url: 'https://i.imgur.com/LFkDe6S.png',
+            preview: true
+        }, {
+            spotId: 6,
+            url: 'https://i.imgur.com/1OAAk9T.png',
+            preview: false
+        }, {
+            spotId: 6,
+            url: 'https://i.imgur.com/Y78r14i.png',
+            preview: false
+        }, {
+            spotId: 6,
+            url: 'https://i.imgur.com/fpzLHGX.png',
+            preview: false
+        }, {
+            spotId: 6,
+            url: 'https://i.imgur.com/Uc1BdXq.png',
+            preview: false
+        }], {})
     },
-    down: async(queryInterface, Sequelize) => {
+    down: async (queryInterface, Sequelize) => {
         options.tableName = 'SpotImages'
         return queryInterface.bulkDelete(options)
     }
